36f673c0b6623cec85837d877827417d8d457587
xiaoqiaoWEB <346043591@qq.com>
2019-06-13 22:16:00 +0800

    mongodb 底层封装

---
+++ mongo/db.js
@@ -0,0 +1,105 @@
+/*
+ * http://mongodb.github.io/node-mongodb-native
+ * http://mongodb.github.io/node-mongodb-native/3.0/api/
+*/
+
+const MongoDb = require('mongodb')
+const MongoClient = MongoDb.MongoClient
+
+const Config = require('./config')
+
+class Db {
+  static getInstance() {
+    if(!Db.instance) {
+      Db.instance = new Db()
+    }
+    return Db.instance
+  }
+
+  constructor () {
+    this.dbClient = ''
+    this.content()
+  }
+
+  content () {
+    return new Promise((resolve, reject) => {
+      if(!this.dbClient) {
+        MongoClient.connect(Config.dbUrl, (err, client) => {
+          if(err) {
+            reject(err)
+          } else {
+            this.dbClient = client.db(Config.dbName)
+            resolve(this.dbClient)
+          }
+        })
+      } else {
+        resolve(this.dbClient)
+      }
+    })
+  }
+
+  find (collectionName, json) {
+    return new Promise((resolve, reject) => {
+      this.content().then((db) => {
+        let result = db.collection(collectionName).find(json);
+        result.toArray((err,docs) => {
+          if(err) {
+            reject(err)
+            return
+          } else {
+            resolve(docs)
+          }
+        })
+      }) 
+    })
+  }
+
+  insert (collectionName, json) {
+    return new Promise((resolve, reject) => {
+      this.content().then((db) => {
+        db.collection(collectionName).insertOne(json, (err,result) => {
+          if(err) {
+            reject(err)
+          } else {
+            resolve(result)
+          }
+        })
+      })
+    })
+  }
+
+  updata (collectionName, json1, json2) {
+    return new Promise((resolve, reject) => {
+      this.content().then((db) => {
+        //db.user.update({},{$set:{}})
+        db.collection(collectionName).updateOne(json1, {$set: json2}, (err,result) => {
+          if(err) {
+            reject(err)
+          } else {
+            resolve(result)
+          }
+        })
+      })
+    })
+  }
+
+  remove (collectionName, json) {
+    return new  Promise((resolve,reject)=>{
+      this.content().then((db) => {
+        db.collection(collectionName).removeOne(json, (err,result) => {
+          if(err) {
+            reject(err)
+          } else {
+            resolve(result)
+          }
+        })
+      })
+    })
+  }
+
+  getObjectId(id){
+    return new ObjectID(id)
+  }
+}
+
+module.exports=Db.getInstance();
---
+++ mongo02.js
@@ -0,0 +1,34 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+
+const router = new Router()
+
+const app = new Koa()
+
+const DB = require('./mongo/db')
+
+router.get('/', async (ctx) => {
+  let res = await DB.find('user',{'name': 'admin'});
+  ctx.body = res;
+})
+
+router.get('/add', async (ctx) => {
+  let res = await DB.insert('user', {'name': 'xiaoqiao', 'age': 18})
+  ctx.body = res;
+})
+
+router.get('/edit', async (ctx) => {
+  let res = await DB.updata('user', {'name': 'xiaoqiao'}, {'age:': 30, 'status': '2'})
+  ctx.body = res;
+})
+
+router.get('/delete', async (ctx) => {
+  let res = await DB.remove('user', {'name': 'xiaoqiao'})
+  ctx.body = res;
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(8000)
